28f9878066f41daf90ea360c6a08ba3c55f9d036
hasan.hawar@powercoders.org <hasan.hawar@powercoders.org>
2020-09-04 16:22:44 +0200

    feat: add the needed selector element to roll the dice and save the score

--- js/app.js
+++ js/app.js
@@ -20,3 +20,9 @@ activePlayer = 0;
 // document.querySelector('#current-' + activePlayer).textContent = dice;
 // document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
 // let x = document.querySelector('#score-0').textContent;
+
+document.querySelector('.dice').style.display = 'none';
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
